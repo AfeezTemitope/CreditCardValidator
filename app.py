907b69e093ade40ba13acff6b0f697430fa3f939
unknown <belloafeez28@gmail.com>
2024-10-06 17:53:37 +0100

    debuggin

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from flask_cors import CORS
 from urls import validate_card_input
 
 app = Flask(__name__)
-CORS(app)
-# CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+# CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+#CORS(app, resources={r"/api/*": {"origins": ["http://localhost:3000"}})
+
 app.register_blueprint(validate_card_input)
 
 
